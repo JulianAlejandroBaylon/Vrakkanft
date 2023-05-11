9da1198d22aacbbf0daa3266dd37b0e846c8dc57
JulianAlejandroBaylon <superhazzard2@gmail.com>
2023-05-11 12:00:24 -0600

    Commit Julian

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import { AboutComponent } from './about/about.component';
+import { BenefitsComponent } from './benefits/benefits.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
-    AboutComponent
+    AboutComponent,
+    BenefitsComponent
   ],
   imports: [
     BrowserModule,
