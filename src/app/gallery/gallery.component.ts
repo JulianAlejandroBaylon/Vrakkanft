cfd54f35e65fd5be401f15acf2745a0c0226c4db
JulianAlejandroBaylon <superhazzard2@gmail.com>
2023-05-15 11:41:45 -0600

    Gallery funcionando

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -3,9 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.css']
+  styleUrls: ['./gallery.component.css'],
 })
 export class GalleryComponent {
-  item: number = 0;
-  selec: number = this.item + 1;
+  x: number = 0;
+  item: number[] = [1, 2, 3, 4, 5, 6, 7];
+  img: string[] = [
+    'assets/2 3.png',
+    'assets/2 4.png',
+    'assets/2 5.png',
+    'assets/2 6.png',
+    'assets/2 7.png',
+    'assets/2 8.png',
+    'assets/2 9.png',
+  ];
+
+  suma(): void {
+    this.x++;
+    if (this.x > 7) {
+      this.x = 1;
+    }
+    console.log(this.x);
+  }
+
+  res(): void {
+    this.x--;
+    if (this.x < 1) {
+      this.x = 7;
+    }
+    console.log(this.x);
+  }
 }
