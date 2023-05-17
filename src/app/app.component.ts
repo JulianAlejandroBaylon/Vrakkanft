6809242e83d02ef979729f0b71865a927a609d5d
JulianAlejandroBaylon <superhazzard2@gmail.com>
2023-05-17 13:53:23 -0600

    Scroll horizontal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,28 +9,27 @@ export class AppComponent {
   title = 'NFT';
   ngOnInit() {
     function reveal() {
-      var slides = document.querySelectorAll(".slides > .slide");
-      var container = document.querySelector(".page-content");
-      var containerScrollLeft = container.scrollLeft;
-      var containerWidth = container.clientWidth;
+      var slides = document.querySelectorAll('.two');
+      var container = document.querySelector('.page-content'); /*Todo el contenido de la pag*/
+      var containerScrollTop = container.scrollTop; /*Valor del scroll vertical (En nuesttro caso horizontal)*/
+      var containerWidth = container.clientWidth; /* Tamaño de la pagina */
 
       for (var i = 0; i < slides.length; i++) {
-        var slide = slides[i];
-        var slideLeft = slide.clientLeft - containerScrollLeft;
+        var slide = slides[i]; /* Guarda la pagina*/
+        var slideLeft = slide.clientLeft - containerScrollTop;
         var slideRight = slideLeft + slide.clientWidth;
-        var isVisible = (slideLeft < containerWidth && slideRight > 0);
+        var isVisible = slideLeft < containerWidth && slideRight < 100 && slideRight > -800;
 
         if (isVisible) {
-          slide.classList.add("active");
+          slide.classList.add('active');
         } else {
-          slide.classList.remove("active");
+          slide.classList.remove('active');
         }
       }
+      console.log("containerWidth", slideRight);
     }
 
-    var container = document.querySelector(".page-content");
-    container.addEventListener("scroll", reveal);
+    var container = document.querySelector('.page-content');
+    container.addEventListener('scroll', reveal);
   }
-
-
 }
