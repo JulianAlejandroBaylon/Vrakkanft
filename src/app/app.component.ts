23c03aa3df5fde8d789c2db9a21d050ac7b15f14
JulianAlejandroBaylon <superhazzard2@gmail.com>
2023-05-19 10:20:23 -0600

    Animacion de scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,107 +1,127 @@
 import { Component } from '@angular/core';
-import { MoveDirection, ClickMode, HoverMode, OutMode, Container, Engine } from "tsparticles-engine";
-import { loadFull } from "tsparticles";
+import {
+  MoveDirection,
+  ClickMode,
+  HoverMode,
+  OutMode,
+  Container,
+  Engine,
+} from 'tsparticles-engine';
+import { loadFull } from 'tsparticles';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-    title = 'NFT';
-    //Id for particles of background
-    id = "tsparticles";
+  title = 'NFT';
+  //Id for particles of background
+  id = 'tsparticles';
 
-    /*JavaScript object */
-    particlesOptions = {
-
-        fpsLimit: 120,
-        particles: {
-            color: {
-                value: "#ffffff",
-            },
-            move: {
-                direction: MoveDirection.none,
-                enable: true,
-                outModes: {
-                    default: OutMode.bounce,
-                },
-                random: false,
-                speed: 1,
-                straight: false,
-            },
-            number: {
-                density: {
-                    enable: true,
-                    area: 800,
-                },
-                value: 80,
-            },
-            opacity: {value: {
-              min: 0.1, // Valor mínimo de opacidad
-              max: 0.5, // Valor máximo de opacidad
-            },
-            animation: {
-              enable: true,
-              speed: 1,
-              minimumValue: 0.1,
-              sync: false,
-            },
-            },
-            shape: {
-                type: "circle",
-            },
-            size: {
-                value: {
-                  min: 1,
-                  max: 5,
-                },
-                animation: {
-                  enable: true,
-                  speed: 3,
-                  minimumValue: 1,
-                  sync: false,
-                },
-            },
+  /*JavaScript object */
+  particlesOptions = {
+    fpsLimit: 120,
+    particles: {
+      color: {
+        value: '#ffffff',
+      },
+      move: {
+        direction: MoveDirection.none,
+        enable: true,
+        outModes: {
+          default: OutMode.bounce,
         },
-        detectRetina: true,
-    };
+        random: false,
+        speed: 1,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: {
+          min: 0.1, // Valor mínimo de opacidad
+          max: 0.5, // Valor máximo de opacidad
+        },
+        animation: {
+          enable: true,
+          speed: 1,
+          minimumValue: 0.1,
+          sync: false,
+        },
+      },
+      shape: {
+        type: 'circle',
+      },
+      size: {
+        value: {
+          min: 1,
+          max: 5,
+        },
+        animation: {
+          enable: true,
+          speed: 3,
+          minimumValue: 1,
+          sync: false,
+        },
+      },
+    },
+    detectRetina: true,
+  };
 
-    particlesLoaded(container: Container): void {
-        console.log(container);
-    }
+  particlesLoaded(container: Container): void {
+    console.log(container);
+  }
 
-    async particlesInit(engine: Engine): Promise<void> {
-        console.log(engine);
+  async particlesInit(engine: Engine): Promise<void> {
+    console.log(engine);
 
-        // Starting from 1.19.0 you can add custom presets or shape here, using the current tsParticles instance (main)
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        await loadFull(engine);
-    }
+    // Starting from 1.19.0 you can add custom presets or shape here, using the current tsParticles instance (main)
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }
 
   ngOnInit() {
-    function reveal() {
-      var slides = document.querySelectorAll('.two');
-      var container = document.querySelector('.page-content'); /*Todo el contenido de la pag*/
-      var containerScrollTop = container.scrollTop; /*Valor del scroll vertical (En nuesttro caso horizontal)*/
-      var containerWidth = container.clientWidth; /* Tamaño de la pagina */
+    const p1 = document.querySelector('.one');
+    const p2 = document.querySelector('.two');
+    const p3 = document.querySelector('.three');
+    const p4 = document.querySelector('.four');
+    const p5 = document.querySelector('.five');
+    const p6 = document.querySelector('.six');
+    const p7 = document.querySelector('.seven');
+    const p8 = document.querySelector('.eight');
 
-      for (var i = 0; i < slides.length; i++) {
-        var slide = slides[i]; /* Guarda la pagina*/
-        var slideLeft = slide.clientLeft - containerScrollTop;
-        var slideRight = slideLeft + slide.clientWidth;
-        var isVisible = slideLeft < containerWidth && slideRight < 100 && slideRight > -800;
+    const carga = (entradas, observador) => {
+      /*console.log("entradas", entradas);
+      console.log("observador", observador);*/
 
-        if (isVisible) {
-          slide.classList.add('active');
-        } else {
-          slide.classList.remove('active');
+      entradas.forEach((entrada)=>{
+        if(entrada.isIntersecting){
+          entrada.target.classList.add('visible');
+        }else{
+          entrada.target.classList.remove('visible');
         }
-      }
-      console.log("containerWidth", slideRight);
+      });
     }
 
-    var container = document.querySelector('.page-content');
-    container.addEventListener('scroll', reveal);
+    const observador = new IntersectionObserver(carga, {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5
+    });
+
+    observador.observe(p1);
+    observador.observe(p2);
+    observador.observe(p3);
+    observador.observe(p4);
+    observador.observe(p5);
+    observador.observe(p6);
+    observador.observe(p7);
+    observador.observe(p8);
   }
 }
