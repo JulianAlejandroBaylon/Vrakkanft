2df6d481e9136bb8932483adcd913ead90478d83
JulianAlejandroBaylon <superhazzard2@gmail.com>
2023-05-20 08:56:29 -0600

    About terminado

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  ran: number [] = [0, 1, 2, 3, 4, 5, 6]
+
+  title: string [] = [
+    'Yericko Vielmas',
+    'Roberto Mejia',
+    'Saul Dominguez',
+    'Eduardo Muñoz',
+    'Yannick Vielmas',
+    'Raul Peinado',
+    'Nancy Altamirano'
+  ]
+
+  text: string[]=[
+    'CEO & founder',
+    'text',
+    'text',
+    'text',
+    'CAO',
+    'COO',
+    'text'
+  ]
+
+  fotos: string []=[
+    'assets/yeri 1.png',
+    'assets/Robert.png',
+    'assets/Saul.png',
+    'assets/Eduardo.png',
+    'assets/Yanncik.png',
+    'assets/Raul.png',
+    'assets/Nancy.png'
+  ]
 }
