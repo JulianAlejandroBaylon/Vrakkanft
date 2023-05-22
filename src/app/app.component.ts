f61e4c5520ee22038b417bc5600f6abb77d97436
JulianAlejandroBaylon <superhazzard2@gmail.com>
2023-05-22 08:46:33 -0600

    Minimos cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -122,10 +122,32 @@ export class AppComponent {
     observador.observe(p7);
     observador.observe(p8);
 
-    addEventListener("wheel", (event) => {});
+  /*  const ethereumButton = document.querySelector('.enableEthereumButton');
+const showAccount = document.querySelector('.showAccount');
 
-    onwheel = (event) => {
-      console.log(event.deltaY);
-    };
+ethereumButton.addEventListener('click', () => {
+  getAccount();
+});
+
+// While awaiting the call to eth_requestAccounts, you should disable
+// any buttons the user can select to initiate the request.
+// MetaMask rejects any additional requests while the first is still
+// pending.
+async function getAccount() {
+  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    .catch((err) => {
+      if (err.code === 4001) {
+        // EIP-1193 userRejectedRequest error
+        // If this happens, the user rejected the connection request.
+        console.log('Please connect to MetaMask.');
+      } else {
+        console.error(err);
+      }
+    });
+  const account = accounts[0];
+  showAccount.innerHTML = account;
+}*/
   }
+
+
 }
